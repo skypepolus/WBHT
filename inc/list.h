20928d4436ef6e218b65afab03cb482b24c7525a
skypepolus <song@youngho.net>
2025-01-04 21:07:26 -0600

    v0.0.2

--- inc/list.h
+++ inc/list.h
@@ -13,7 +13,7 @@ struct list
 	struct link tail;
 };
 
-static inline void list_initial(struct list* list)
+static inline void list_initial(register struct list* list)
 {
 	list->head.previous = NULL;
 	list->head.next = &list->tail;
@@ -21,28 +21,28 @@ static inline void list_initial(struct list* list)
 	list->tail.next = NULL;
 }
 
-static inline void link_insert(struct link* link, struct link* next)
+static inline void link_insert(register struct link* link, register struct link* next)
 {
-	struct link* previous = next->previous;
+	register struct link* previous = next->previous;
 	previous->next = link;
 	link->previous = previous;
 	next->previous = link;
 	link->next = next;
 }
 
-static inline void link_add(struct link* previous, struct link* link)
+static inline void link_add(register struct link* previous, register struct link* link)
 {
-	struct link* next = previous->next;
+	register struct link* next = previous->next;
 	previous->next = link;
 	link->previous = previous;
 	next->previous = link;
 	link->next = next;
 }
 
-static inline void link_remove(struct link* link)
+static inline void link_remove(register struct link* link)
 {
-	struct link* previous = link->previous;
-	struct link* next = link->next;
+	register struct link* previous = link->previous;
+	register struct link* next = link->next;
 	previous->next = next;
 	next->previous = previous;
 }
